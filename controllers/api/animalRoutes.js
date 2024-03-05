3f4510881ac5c29c6d46387e9600c30f1df3422e
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-03-04 20:08:28 -0500

    added animals routes(commented), added user routes(authorization commented)

--- controllers/api/animalRoutes.js
+++ controllers/api/animalRoutes.js
@@ -1,1 +1,30 @@
 //Ian
+// const router = require('express').Router();
+// const { animals } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+//test get
+// router.get('/', /*withAuth,*/ async (req, res) => {
+//     try{
+//         const allAnimals = await animals.findAll({
+//             attributes: {
+//                 exclude: ['id']
+//             }
+//         });
+//         res.status(200).json(allAnimals);
+//     } catch(err) {
+//         res.status(400).json(err);
+//     }
+// });
+
+// router.get('/:id', /*withAuth,*/ async (req, res) => {
+//     try {
+//         const oneAnimal = await animals.findByPK(req.params.id, {
+//             attributes: {
+//                 exclude: ['id']
+//             }
+//         });
+//     } catch {
+
+//     }
+// });
