7d9d79423dc0f11ce1ee8463ed3c35c083052937
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-03-04 20:47:33 -0500

    added Tag Routes

--- controllers/api/tagRoutes.js
+++ controllers/api/tagRoutes.js
@@ -1,1 +1,43 @@
 //Ian
+const router = require('express').Router();
+const { tag } = require('../../model');
+// const withAuth = require('../../utils/auth');
+
+//test get
+router.get('/', /*withAuth,*/ async (req, res) => {
+    try {
+        const allTags = await tag.findAll();
+        res.status(200).json(allTags);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/', /*withAuth,*/ async (req, res) => {
+    try {
+        const newTag = await tag.create(req.body);
+
+        res.status(200).json(newTag);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const deleteTag = await tag.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!deleteTag) {
+            res.status(404).json({ message: 'no tag found with this id' });
+            return;
+        }
+
+        res.status(200).json(deleteTag);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
