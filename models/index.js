4942fffd4effc71befc8afd2fa87b914b06b8522
unknown <jozsua901@gmail.com>
2024-03-05 20:42:36 -0500

    added index

--- models/index.js
+++ models/index.js
@@ -10,18 +10,36 @@ animals.hasMany(comments, {
     onDelete: 'CASCADE'
 });
 
+comments.belongsTo(animals, {
+    foreignKey: 'animal_id'
+})
+
 users.hasMany(comments, {
     foreignKey: 'users_id',
     onDelete: 'CASCADE',
     
 });
 
+comments.belongsTo(users, {
+    foreignKey: 'users_id'
+})
+
 tags.belongsToMany(comments, {
-    through: {
-        model: animals,
-        unique: false
-    },
-    as: 'comments_tags'
+    // through: {
+    //     model: animals,
+    //     unique: false
+    // },
+    // as: 'comments_tags'
 });
+
+
+comments.belongsToMany(tags, {
+    // through: {
+    //     model: animals,
+    //     unique: false
+    // },
+    // as: 'tags_comments'
+});
+
 
 module.exports = { animals, comments, tags, users};
