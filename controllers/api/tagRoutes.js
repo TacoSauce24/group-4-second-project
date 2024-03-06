a6bcab41ea00cd08be549cc3c619c46727f45d36
Spikey1289 <110435462+Spikey1289@users.noreply.github.com>
2024-03-05 21:22:15 -0500

    boop

--- controllers/api/tagRoutes.js
+++ controllers/api/tagRoutes.js
@@ -23,7 +23,7 @@ router.post('/', /*withAuth,*/ async (req, res) => {
     }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete('/:id', /*withAuth,*/ async (req, res) => {
     try {
         const deleteTag = await tags.destroy({
             where: {
