a2c28e35094fbd32bec2cf36ca55e1f0355abf75
Lea Arcidiacono <live2run_1980@yahoo.com>
2024-03-07 20:20:00 -0500

    fixed views main issue

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -9,9 +11,11 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create();
 
 const sess = {
-    secret: 'login',
+    secret: process.env.SECRET,    
     cookie: {
         maxAge: 300000,
         httpOnly: true,
@@ -26,6 +30,9 @@ const sess = {
 };
 
 app.use(session(sess));
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
